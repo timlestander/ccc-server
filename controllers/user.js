85351f2d02bd31359e1f0adbe61fa55ce4b520b0
Ola Jigin <ola.jigin@gmail.com>
2018-07-03 22:10:13 +0200

    added update user backend for hh and ok

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,20 @@ const getAllUsers = async (req, res) => {
   res.send(users);
 };
 
+const getUserById = async (req, res) => {
+  const user = await userService.getUserById(req.params.id);
+  res.send(user);
+}
+
+const updateUser = async (req, res) => {
+  //TODO sanity check body
+  await userService.updateUser(req.body);
+  res.send({
+    success: true
+  });
+}
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  getUserById,
+  updateUser
 };
--- router.js
+++ router.js
@@ -1,15 +1,17 @@
-const authController = require('./controllers/auth');
-const authMiddleware = require('./middlewares/auth');
-const userController = require('./controllers/user');
-const pollController = require('./controllers/poll');
+const authController = require("./controllers/auth");
+const authMiddleware = require("./middlewares/auth");
+const userController = require("./controllers/user");
+const pollController = require("./controllers/poll");
 
 module.exports.set = app => {
   // Public routes
-  app.post('/login', authController.login);
-  app.post('/register', authController.register);
+  app.post("/login", authController.login);
+  app.post("/register", authController.register);
 
   // Protected routes
-  app.get('/users', authMiddleware.checkAuth, userController.getAllUsers);
-  app.get('/polls', authMiddleware.checkAuth, pollController.getAllPolls);
-  app.get('/poll/:id', authMiddleware.checkAuth, pollController.getPollById);
+  app.get("/users", authMiddleware.checkAuth, userController.getAllUsers);
+  app.get("/user/:id", authMiddleware.checkAuth, userController.getUserById);
+  app.put("/user", userController.updateUser);
+  app.get("/polls", authMiddleware.checkAuth, pollController.getAllPolls);
+  app.get("/poll/:id", authMiddleware.checkAuth, pollController.getPollById);
 };
